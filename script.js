99bf636b5817ee0484825015a292cbaeecf37aa0
malakshaer <shaermk06@gmail.com>
2022-09-12 22:24:59 +0300

    change in js file

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const phone = document.getElementById("phone");
 const form = document.getElementById("form");
 const submit = document.getElementById("submit");
 const message = document.getElementById("textarea");
+const inputContainer = document.getElementsByClassName("input-container");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -17,16 +18,14 @@ function checkInput() {
   const phoneValue = phone.value.trim();
   const messageValue = message.value.trim();
 
-  if (nameValue === "" || nameValue == null) {
-    setErrorFor(fullName, "name can not be blank");
-  } else if (nameValue.length < 5) {
+  //name
+  if (nameValue.length < 5) {
     setErrorFor(fullName, "full name must be more than five character");
   } else {
     setSuccessFor(fullName);
   }
-  if (emailValue === "") {
-    setErrorFor(email, "Email cannot be blank");
-  } else if (!isEmail(emailValue)) {
+  //email
+  if (!isEmail(emailValue)) {
     setErrorFor(email, "Not a valid email");
   } else if (!"^[A-Za-z0-9_-]*3@^[A-Za-z.]*5".test(email)) {
     setErrorFor(
@@ -37,9 +36,8 @@ function checkInput() {
     setSuccessFor(email);
   }
 
-  if (phoneValue === "" || phoneValue == null) {
-    setErrorFor(phone, "phone can not be blank");
-  } else if (!"^+[0-9]*3/[0-9]*8".test(phone)) {
+  //phone number
+  if (!"^+[0-9]*3/[0-9]*8".test(phone)) {
     setErrorFor(
       phone,
       "phone must start with code +961 form example and must have 8 digit after"
@@ -48,6 +46,7 @@ function checkInput() {
     setSuccessFor(phone);
   }
 
+  //text message
   if (messageValue.length < 99) {
     setErrorFor(message, "message must be min 100 in length");
   } else {
@@ -60,7 +59,7 @@ function setErrorFor(input, message) {
   const small = inputContainer.querySelector("#small");
 
   inputContainer.className = "input-container error";
-  small.innerText = message;
+  small.innerHTML = message;
 }
 
 function setSuccessFor(input) {
